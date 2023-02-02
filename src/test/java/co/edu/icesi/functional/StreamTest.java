74c11b21d1e2b0812672ea273de921094ed5ea56
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2023-02-02 15:52:54 -0500

    Testing

--- src/test/java/co/edu/icesi/functional/StreamTest.java
+++ src/test/java/co/edu/icesi/functional/StreamTest.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StreamTest {
 
@@ -14,6 +18,7 @@ public class StreamTest {
     @Test
     public void testStreamMap(){
         List<String> lastNames = defaultIcesiUsers().stream().map(IcesiUser::getLastName).toList();
+        assertEquals(lastNames, List.of("Doe", "Doenson", "Doe", "Doe"));
 
 
     }
@@ -21,6 +26,9 @@ public class StreamTest {
     @Test
     public void testStreamFilter(){
         List<IcesiUser> filteredUsers = defaultIcesiUsers().stream().filter(IcesiUser::isActive).toList();
+        assertTrue(filteredUsers.stream().allMatch(IcesiUser::isActive));
+
+
 
     }
 
@@ -28,16 +36,24 @@ public class StreamTest {
     public void testStreamFlatMap(){
         List<List<IcesiUser>> listOfListOfIcesiUsers = List.of(defaultIcesiUsers(),defaultIcesiUsers());
         List<IcesiUser> result = listOfListOfIcesiUsers.stream().flatMap(Collection::stream).toList();
+        System.out.println(listOfListOfIcesiUsers);
+        System.out.println(result);
+        assertEquals(listOfListOfIcesiUsers.get(0).get(0), result.get(0));
+
+
+
     }
 
     @Test
     public void testStreamReduce(){
         String names = defaultIcesiUsers().stream().map(IcesiUser::getName).reduce("", (concatenatedValue, nextValue) -> concatenatedValue.concat("," + nextValue));
+        assertEquals(names, ",John,John,John,John");
 
     }
 
     @Test
     public void testStream(){
+
 
     }
 
