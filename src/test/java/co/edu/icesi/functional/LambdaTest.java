238648856fc44155747052a0e8c7dad72fa73dc7
Keren <kerenlopezcordoba@outlook.es>
2023-02-02 15:55:11 -0500

    Assert were added

--- src/test/java/co/edu/icesi/functional/LambdaTest.java
+++ src/test/java/co/edu/icesi/functional/LambdaTest.java
@@ -10,29 +10,30 @@ public class LambdaTest {
 
     private LambdaHelper lambdaHelper = new LambdaHelper();
 
+//4 TIPOS DE FUNCTIONAL INTERFACES
 
+//se debe usar parentesis para varios parametros, van vacíos cuando no se recibe nada (x,y,z) -> x.concat(" world!"
+//Evitar usar corchetes para funciones largas, mejor hacerlas aparte
     @Test
     public void testFunction(){
         String result = lambdaHelper.stringFunctionMethod ( x -> x.concat(" world!") );
         assertEquals("Hello world!", result);
     }
-
+//No reciben parametros, pero sí retornan valores, son generadoras de valores
     @Test
     public void testSupplier(){
         String result = lambdaHelper.stringSupplierMethod( () -> "Hello world!");
         assertEquals("Hello world!", result);
     }
-
+//Funciones que procesan valores, pero no tienen retorno
     @Test
     public void testConsumer(){
         lambdaHelper.consumerMethod(System.out::println);
     }
-
+//Para colocar condiciones, las valida
     @Test
     public void testPredicate(){
         assertTrue(lambdaHelper.predicateMethod( x -> x.equals("Hello world!") ));
     }
-
-
 
 }
--- src/test/java/co/edu/icesi/functional/StreamTest.java
+++ src/test/java/co/edu/icesi/functional/StreamTest.java
@@ -8,28 +8,42 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class StreamTest {
 
+//forma de llamar funciones cuando el parametro corresponde con el valor que espero mapear
+// IcesiUser::getLastName, clase::metodo
+//Si estoy dentro de la misma clase es this::metodo
 
+//FUNCIONES DE MAPEO, RECIBO UN VALOR Y RETORNO UN VALOR. SON PARA COLECCIONES(LISTAS, SETS)
     @Test
     public void testStreamMap(){
         List<String> lastNames = defaultIcesiUsers().stream().map(IcesiUser::getLastName).toList();
-
-
+        var lastName1 = lastNames.get(0);
+        var lastName2 = lastNames.get(1);
+        var lastName3 = lastNames.get(2);
+        var lastName4 = lastNames.get(3);
+        assertEquals("Doe", lastName1);
+        assertEquals("Doenson", lastName2);
+        assertEquals("Lopez", lastName3);
+        assertEquals("Cordoba", lastName4);
+        assertTrue(lastNames.size()==4);
     }
-
+//filtra usuarios activos, filter recibe una función que retorna una funcion
     @Test
     public void testStreamFilter(){
         List<IcesiUser> filteredUsers = defaultIcesiUsers().stream().filter(IcesiUser::isActive).toList();
 
     }
-
+//A partir de listas diferentes, las une en una sola, no elimina repetidos, con distinct() pero hay que sobreescribir el equals y el hash antes es buena practica
     @Test
     public void testStreamFlatMap(){
         List<List<IcesiUser>> listOfListOfIcesiUsers = List.of(defaultIcesiUsers(),defaultIcesiUsers());
         List<IcesiUser> result = listOfListOfIcesiUsers.stream().flatMap(Collection::stream).toList();
     }
-
+//Concatena todos los nombres, separados por coma y los coloca en un string
     @Test
     public void testStreamReduce(){
         String names = defaultIcesiUsers().stream().map(IcesiUser::getName).reduce("", (concatenatedValue, nextValue) -> concatenatedValue.concat("," + nextValue));
@@ -51,9 +65,11 @@ public class StreamTest {
         icesiUsers.add(icesiUser2);
         IcesiUser icesiUser3 = defaultIcesiUser();
         icesiUser3.setActive(false);
+        icesiUser3.setLastName("Lopez");
         icesiUsers.add(icesiUser3);
         IcesiUser icesiUser4 = defaultIcesiUser();
         icesiUser4.setId(4);
+        icesiUser4.setLastName("Cordoba");
         icesiUsers.add(icesiUser4);
         return icesiUsers;
     }
