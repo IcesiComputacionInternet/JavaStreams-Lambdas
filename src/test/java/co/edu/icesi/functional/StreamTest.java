ad20d69b9dea5a0b871f927297c81dd63f29003c
Luis Miguel Ossa Arias <luismiguelossaarias05@gmail.com>
2023-02-02 20:26:03 -0500

    First commit

--- src/test/java/co/edu/icesi/functional/StreamTest.java
+++ src/test/java/co/edu/icesi/functional/StreamTest.java
@@ -8,14 +8,24 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StreamTest {
 
 
     @Test
-    public void testStreamMap(){
+    public void testCheckOrderStreamMap(){
         List<String> lastNames = defaultIcesiUsers().stream().map(IcesiUser::getLastName).toList();
 
+        String lastName1 = "Trochez";
+        String lastName2 = "Lopez";
+        String lastName3 = "Corrales";
+        String lastName4 = "Martinez";
 
+        assertEquals(lastName1, lastNames.get(0));
+        assertEquals(lastName2, lastNames.get(1));
+        assertEquals(lastName3, lastNames.get(2));
+        assertEquals(lastName4, lastNames.get(3));
     }
 
     @Test
@@ -45,16 +55,23 @@ public class StreamTest {
     private List<IcesiUser> defaultIcesiUsers() {
         List<IcesiUser> icesiUsers = new ArrayList<>();
         IcesiUser icesiUser1 = defaultIcesiUser();
+        icesiUser1.setLastName("Trochez");
         icesiUsers.add(icesiUser1);
+
         IcesiUser icesiUser2 = defaultIcesiUser();
-        icesiUser2.setLastName("Doenson");
+        icesiUser2.setLastName("Lopez");
         icesiUsers.add(icesiUser2);
+
         IcesiUser icesiUser3 = defaultIcesiUser();
         icesiUser3.setActive(false);
+        icesiUser3.setLastName("Corrales");
         icesiUsers.add(icesiUser3);
+
         IcesiUser icesiUser4 = defaultIcesiUser();
         icesiUser4.setId(4);
+        icesiUser4.setLastName("Martinez");
         icesiUsers.add(icesiUser4);
+
         return icesiUsers;
     }
 
